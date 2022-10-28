e40185a3bf39f1329ef9ed7e07bb47280b7e3df0
ghjk0498 <ghjk0498@gmail.com>
2022-10-28 17:21:31 +0900

    Update TestApplication.java

--- src/main/java/spring/TestApplication.java
+++ src/main/java/spring/TestApplication.java
@@ -1,11 +1,61 @@
 package spring;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Optional;
 
 public class TestApplication {
 
 	public static void main(String[] args) {
-		ProcessHandle.allProcesses().forEach(process -> System.out.println(processDetails(process)));
+//		ProcessHandle.allProcesses().forEach(process -> System.out.println(processDetails(process)));
+		String[] strArr = new String[1];
+		strArr[0] = "C:\\Program Files\\Notepad++\\notepad++.exe";
+
+		runProcess(strArr);
+//		ProcessHandle.allProcesses().forEach(process -> {
+//			try {
+//				String filename = new File(process.info().command().get()).getName();
+//				if (filename.equals("notepad++.exe")) {
+//					processKill(process);
+//				}
+//			} catch (Exception e) {
+////				e.printStackTrace();
+//			}
+//		});
+	}
+
+	private static void killProcess(ProcessHandle process) {
+		Long parentProcessId = process.parent().map(ProcessHandle::pid).get();
+		ProcessHandle parentProcess = ProcessHandle.of(parentProcessId).get();
+		String parentProcessCommand = parentProcess.info().command().get();
+		String processCommand = process.info().command().get();
+
+//		System.out.println(processCommand);
+//		System.out.println(parentProcessCommand);
+		if (process.info().command().get().equals(parentProcessCommand)) {
+			parentProcess.destroy();
+		} else {
+			process.destroy();
+		}
+	}
+
+	private static void runProcess(String[] command) {
+		try {
+			Process process = Runtime.getRuntime().exec(command);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static boolean findProcess(String name) {
+		ProcessHandle.allProcesses().filter(process -> {
+			if (name.equals(new File(process.info().command().get()).getName()) {
+				return true;
+			} else {
+				return false;
+			}
+		})
 	}
 
 	private static String processDetails(ProcessHandle process) {
@@ -14,7 +64,6 @@ public class TestApplication {
 	            text(process.parent().map(ProcessHandle::pid)),
 	            text(process.info().user()),
 	            text(process.info().startInstant()),
-//	            text(process.info().commandLine())
 	            text(process.info().command())
 			);
 	}
