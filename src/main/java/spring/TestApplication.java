7123712ccc95ff38b17e54bf25727798a98e827c
ghjk0498 <80383559+ghjk0498@users.noreply.github.com>
2022-11-05 18:31:26 +0900

    프로세스 유지 기능

--- src/main/java/spring/TestApplication.java
+++ src/main/java/spring/TestApplication.java
@@ -2,17 +2,36 @@ package spring;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class TestApplication {
 
 	public static void main(String[] args) {
 //		ProcessHandle.allProcesses().forEach(process -> System.out.println(processDetails(process)));
-		String[] strArr = new String[1];
-		strArr[0] = "C:\\Program Files\\Notepad++\\notepad++.exe";
 
-		runProcess(strArr);
+//		String[] strArr = new String[1];
+//		strArr[0] = "C:\\Program Files\\Notepad++\\notepad++.exe";
+//		runProcess(strArr);
+
+		while (true) {
+			try {
+				ProcessHandle processHandle = findProcess("notepad.exe");
+				if (processHandle != null) {
+					// 테스트 용
+//					killProcess(processHandle);
+				} else {
+					System.out.println("Execute");
+					runProcess(new String[] { "C:\\Windows\\System32\\notepad.exe" });
+//					runProcess(args);
+				}
+
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
 //		ProcessHandle.allProcesses().forEach(process -> {
 //			try {
 //				String filename = new File(process.info().command().get()).getName();
@@ -25,18 +44,23 @@ public class TestApplication {
 //		});
 	}
 
-	private static void killProcess(ProcessHandle process) {
-		Long parentProcessId = process.parent().map(ProcessHandle::pid).get();
+	private static void killProcess(ProcessHandle processHandle) {
+		Long parentProcessId = processHandle.parent().map(ProcessHandle::pid).orElse(-1L);
+		if (parentProcessId.equals(-1L)) {
+			processHandle.destroy();
+			return;
+		}
+
 		ProcessHandle parentProcess = ProcessHandle.of(parentProcessId).get();
 		String parentProcessCommand = parentProcess.info().command().get();
-		String processCommand = process.info().command().get();
+		String processCommand = processHandle.info().command().get();
 
 //		System.out.println(processCommand);
 //		System.out.println(parentProcessCommand);
-		if (process.info().command().get().equals(parentProcessCommand)) {
+		if (processCommand.equals(parentProcessCommand)) {
 			parentProcess.destroy();
 		} else {
-			process.destroy();
+			processHandle.destroy();
 		}
 	}
 
@@ -48,14 +72,15 @@ public class TestApplication {
 		}
 	}
 
-	private static boolean findProcess(String name) {
-		ProcessHandle.allProcesses().filter(process -> {
-			if (name.equals(new File(process.info().command().get()).getName()) {
-				return true;
-			} else {
-				return false;
+	private static ProcessHandle findProcess(String name) {
+		ProcessHandle result = null;
+		for (ProcessHandle processHandle : ProcessHandle.allProcesses().collect(Collectors.toList())) {
+			String path = processHandle.info().command().orElse("");
+			if (name.toLowerCase().equals(new File(path).getName().toLowerCase())) {
+				result = processHandle;
 			}
-		})
+		}
+		return result;
 	}
 
 	private static String processDetails(ProcessHandle process) {
